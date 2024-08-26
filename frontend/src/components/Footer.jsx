cb9eb9707e4c2a6336332740a950b624c02df749
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-08-25 20:10:09 -0400

    added footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="flex bg-neutral-200 items-center justify-center p-2 ">
+            <p className="text-center font-light">Copyright © {new Date().getFullYear()} - All right reserved by Garry's Florists Ltd - Website Made by Oussama H.</p>
+        </footer>
+    )
+}
+
+export default Footer
